6e7b0d211669a3cf1aa27029c3aab77854c6c563
Josef Hrebec <josefhrebec02@gmail.com>
2023-06-23 15:02:19 +0200

    Basic express init

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const mongoDB = process.env.PASSWD;
+mongoose.connect(mongoDB);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(3000, () => {
+  console.log("Server listens at port 3000");
+});
+
+module.exports = app;
